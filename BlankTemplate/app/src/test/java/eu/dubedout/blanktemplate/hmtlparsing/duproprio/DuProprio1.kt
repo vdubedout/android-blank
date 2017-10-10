0779387baee0f921429b293921543355cdcbe3d0
Vincent Dubedout <vincent.dubedout@gmail.com>
2017-10-10 09:21:39 -0400

    add url

--- BlankTemplate/app/src/test/java/eu/dubedout/blanktemplate/hmtlparsing/duproprio/DuProprio1.kt
+++ BlankTemplate/app/src/test/java/eu/dubedout/blanktemplate/hmtlparsing/duproprio/DuProprio1.kt
@@ -3,6 +3,7 @@ package eu.dubedout.blanktemplate.hmtlparsing.duproprio
 import eu.dubedout.blanktemplate.processor.PhysicalBuildingCaracteristics
 
 class DuProprio1 {
+    val url = "https://duproprio.com/fr/montreal/rosemont-la-petite-patrie/condo-a-vendre/hab-3231-avenue-du-mont-royal-est-766954"
     val extractedData = PhysicalBuildingCaracteristics(
             title = "Condo à vendre",
             address = "3231, Avenue du Mont-Royal Est\n" +
